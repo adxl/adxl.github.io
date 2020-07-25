85af0dbf48cf517fd4118da4d6f17354ca5712f2
Adel Senhadji <adelsenhadjii@gmail.com>
2020-07-25 18:53:41 +0200

    add 1 by 1 animation for projects

--- script.js
+++ script.js
@@ -4,43 +4,57 @@ function scrollToAbout() {
     about.scrollIntoView({ behavior: "smooth" });
 }
 
+function gotoWtrk() {
+    var project = document.getElementById("p-wtrk");
+    project.scrollIntoView({ behavior: "smooth" });
+    $("#p-wtrk .picture").fadeTo("fast", 1)
+    $("#p-wtrk .picture").animate({ left: 30 })
+}
 
-// $(window).scroll(function () {
-//     $('.onfoot1').each(function () {
-//         if ($(window).scrollTop() + $(window).height() - scrollOffset > $(this).position().top) {
-//             if ($(this).position().left < $(this).data("scrollTo")) {
-//                 $(this).clearQueue().animate({
-//                     left: $(this).data("scrollTo")
-//                 });
-//             }
-//         } else {
-//             if ($(this).position().left > 0) {
-//                 $(this).clearQueue().animate({
-//                     left: 0
-//                 });
-//             }
-//         }
-//     });
-// });
+function gotoLife() {
+    var project = document.getElementById("p-life");
+    project.scrollIntoView({ behavior: "smooth" });
+    $("#p-life .picture").fadeTo("fast", 1)
+    $("#p-life .picture").animate({ left: 30 })
+}
+
+function gotoKnights() {
+    var project = document.getElementById("p-knights");
+    project.scrollIntoView({ behavior: "smooth" });
+    $("#p-knights .picture").fadeTo("fast", 1)
+    $("#p-knights .picture").animate({ left: 30 })
+}
+
+function gotoWebuy() {
+    var project = document.getElementById("p-webuy");
+    project.scrollIntoView({ behavior: "smooth" });
+    $("#p-webuy .picture").fadeTo("fast", 1)
+    $("#p-webuy .picture").animate({ left: 30 })
+}
+
+function gotoForum() {
+    var project = document.getElementById("p-forum");
+    project.scrollIntoView({ behavior: "smooth" });
+    $("#p-forum .picture").fadeTo("fast", 1)
+    $("#p-forum .picture").animate({ left: 30 })
+}
+
 
 
 $(window).scroll(function () {
     $('.picture').each(function () {
         if ($(window).scrollTop() + $(window).height() - 200 > $(this).position().top) {
-            $(this).clearQueue().fadeTo("fast", 1)
-            $(this).clearQueue().animate({
-                left: 30
-            }
-            )
+
+
 
 
         } else {
-            if ($(this).css("opacity") > 0) {
-                $(this).clearQueue().fadeTo("fast", 0)
-                $(this).clearQueue().animate({
-                    left: -500
-                })
-            };
+            // if ($(this).css("opacity") > 0) {
+            //     $(this).clearQueue().fadeTo("fast", 0)
+            //     $(this).clearQueue().animate({
+            //         left: -500
+            //     })
+            // };
         }
 
     })
