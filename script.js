3ea7ef6aab8f6f036de456c0e4d6b85b3e53b9d2
Adel Senhadji <adelsenhadjii@gmail.com>
2020-09-11 23:54:22 +0200

    update script.js

--- script.js
+++ script.js
@@ -5,83 +5,77 @@ function gotoIntro() {
 
 function gotoAbout() {
     // console.log("scrolled");
-    var sec = document.getElementById("about-sec");
+    let sec = document.getElementById("about-sec");
     sec.scrollIntoView({ behavior: "smooth" });
 }
 
 function gotoEdu() {
     // console.log("scrolled");
-    var sec = document.getElementById("edu-sec");
+    let sec = document.getElementById("edu-sec");
     sec.scrollIntoView({ behavior: "smooth" });
 }
 function gotoSkills() {
     // console.log("scrolled");
-    var sec = document.getElementById("skills-sec");
+    let sec = document.getElementById("skills-sec");
     sec.scrollIntoView({ behavior: "smooth" });
 }
 
 function gotoProjects() { //
-    var sec = document.getElementById("projects-sec");
+    let sec = document.getElementById("projects-sec");
     sec.scrollIntoView({ behavior: "smooth" });
 }
 
-function gotoWtrk() {
-    var project = document.getElementById("p-wtrk");
-    project.scrollIntoView({ behavior: "smooth" });
-    $("#p-wtrk .picture").clearQueue().animate({ left: 30 }, 600)
-    $("#p-wtrk .picture").clearQueue().fadeTo("slow", 1)
-}
+// function gotoWtrk() {
+//     var project = document.getElementById("p-wtrk");
+//     project.scrollIntoView({ behavior: "smooth" });
+//     $("#p-wtrk .picture").clearQueue().animate({ left: 30 }, 600)
+//     $("#p-wtrk .picture").clearQueue().fadeTo("slow", 1)
+// }
 
-function gotoLife() {
-    var project = document.getElementById("p-life");
-    project.scrollIntoView({ behavior: "smooth" });
-    $("#p-life .picture").clearQueue().animate({ left: 30 }, 600)
-    $("#p-life .picture").clearQueue().fadeTo("slow", 1)
-}
+// function gotoLife() {
+//     var project = document.getElementById("p-life");
+//     project.scrollIntoView({ behavior: "smooth" });
+//     $("#p-life .picture").clearQueue().animate({ left: 30 }, 600)
+//     $("#p-life .picture").clearQueue().fadeTo("slow", 1)
+// }
 
-function gotoKnights() {
-    var project = document.getElementById("p-knights");
-    project.scrollIntoView({ behavior: "smooth" });
-    $("#p-knights .picture").clearQueue().animate({ left: 30 }, 600)
-    $("#p-knights .picture").clearQueue().fadeTo("slow", 1)
-}
+// function gotoKnights() {
+//     var project = document.getElementById("p-knights");
+//     project.scrollIntoView({ behavior: "smooth" });
+//     $("#p-knights .picture").clearQueue().animate({ left: 30 }, 600)
+//     $("#p-knights .picture").clearQueue().fadeTo("slow", 1)
+// }
 
-function gotoWebuy() {
-    var project = document.getElementById("p-webuy");
-    project.scrollIntoView({ behavior: "smooth" });
-    $("#p-webuy .picture").clearQueue().animate({ left: 30 }, 600)
-    $("#p-webuy .picture").clearQueue().fadeTo("slow", 1)
-}
+// function gotoWebuy() {
+//     var project = document.getElementById("p-webuy");
+//     project.scrollIntoView({ behavior: "smooth" });
+//     $("#p-webuy .picture").clearQueue().animate({ left: 30 }, 600)
+//     $("#p-webuy .picture").clearQueue().fadeTo("slow", 1)
+// }
 
-function gotoForum() {
-    var project = document.getElementById("p-forum");
-    project.scrollIntoView({ behavior: "smooth" });
-    $("#p-forum .picture").clearQueue().animate({ left: 30 }, 600)
-    $("#p-forum .picture").clearQueue().fadeTo("slow", 1)
-}
+// function gotoForum() {
+//     var project = document.getElementById("p-forum");
+//     project.scrollIntoView({ behavior: "smooth" });
+//     $("#p-forum .picture").clearQueue().animate({ left: 30 }, 600)
+//     $("#p-forum .picture").clearQueue().fadeTo("slow", 1)
+// }
 
 function gotoResume() {
     var sec = document.getElementById("resume-sec");
     sec.scrollIntoView({ behavior: "smooth" });
 }
 
+// $(window).scroll(function () {
+//     $('.picture').each(function () {
+//         if ($(window).scrollTop() + $(window).height() - 200 > $(this).position().top) {
 
-$(window).scroll(function () {
-    $('.picture').each(function () {
-        if ($(window).scrollTop() + $(window).height() - 200 > $(this).position().top) {
-
-
-
-
-        } else {
-            // if ($(this).css("opacity") > 0) {
-            //     $(this).clearQueue().fadeTo("fast", 0)
-            //     $(this).clearQueue().animate({
-            //         left: -500
-            //     })
-            // };
-        }
-
-    })
-
-})
+//         } else {
+//             if ($(this).css("opacity") > 0) {
+//                 $(this).clearQueue().fadeTo("fast", 0)
+//                 $(this).clearQueue().animate({
+//                     left: -500
+//                 })
+//             };
+//         }
+//     })
+// })
