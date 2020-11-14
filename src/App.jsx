c702f287a0f8f739fb8200ba4149042daea06f9d
Adel Senhadji (adxl) <adelsenhadjii@gmail.com>
2020-11-14 15:03:11 +0100

    Add basic homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+
+import Header from './components/Header';
+import Section from './components/Section';
 
 export default function App() {
+	const [section, setSection] = useState('Home');
+
 	return (
-		<>
-			<h1>Hi</h1>
-		</>
+		<Container fluid className="d-flex flex-column h-100">
+			<Header setSection={setSection} />
+			<Section section={section} />
+		</Container>
 	);
 }
