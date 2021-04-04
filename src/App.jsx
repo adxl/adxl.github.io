527dffd003ded86656b3cd02dcaeabc07228085d
Adel Senhadji (adxl) <adelsenhadjii@gmail.com>
2021-04-04 17:26:36 +0200

    Add education timeline

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Section from './components/Section';
 import Footer	 from './components/Footer';
 
 export default function App() {
-	const [section, setSection] = useState('home');
+	const [section, setSection] = useState('edu');
 
 	return (
 		<Container id="main" fluid className="d-flex flex-column h-100 p-0 position-relative">
--- src/components/sections/Education.jsx
+++ src/components/sections/Education.jsx
@@ -3,8 +3,45 @@ import Container from 'react-bootstrap/Container';
 
 export default function Education() {
 	return (
-		<Container className="text-center">
-			<h1>Education</h1>
+		<Container>
+			<div>
+				<div>
+					<h1 className="mb-4">FORMATION</h1>
+					<ul id="edu-list">
+						<li>
+							<p className="d-flex align-items-center">
+								<span className="circle" />
+								2020-2021
+							</p>
+							<p>
+								Bachelor en Ingénierie du Web - ESGI, France
+							</p>
+						</li>
+						<li>
+							<p className="d-flex align-items-center">
+								<span className="circle" />
+								2016-2019
+							</p>
+							<p>
+								Licence en Informatique - Université d&apos;Oran 1, Algérie
+							</p>
+						</li>
+						<li>
+							<p className="d-flex align-items-center">
+								<span className="circle" />
+								Juin 2016
+							</p>
+							<p>
+								Baccalauréat Scientifique - Lycée Colonel Lotfi, Algérie
+							</p>
+						</li>
+					</ul>
+				</div>
+				<div id="mobile-curve-container">
+					<div id="mobile-curve" />
+				</div>
+
+			</div>
 		</Container>
 	);
 }
