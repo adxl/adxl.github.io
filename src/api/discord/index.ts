37b4f7a11942db9631279e40be554974936e6484
Adel Sen <adelsenhadjii@gmail.com>
2023-09-29 17:46:33 +0200

    Feat: replace beacon with fetch

--- src/api/discord/index.ts
+++ src/api/discord/index.ts
@@ -1,10 +1,13 @@
 const URL = 'https://discordapp.com/api/webhooks/1156577092255883306/Ekafu0SHRJzkX7AigRwkKM_0NEw8dCVVbV9YC2Oj4855826qP1ojjtK2DuGJ7ct1A-B1'
 
 const alertSentinelle = (content: string): void => {
-  const body = new Blob([JSON.stringify({ content })], {
-    type: 'application/json'
+  void fetch(URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ content })
   })
-  navigator.sendBeacon(URL, body)
 }
 
 export const alertVisit = (): void => {
